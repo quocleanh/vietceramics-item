93573b5361faad55940c4774a267d3423e48e6f8
Quốc Lê <quoc.leanh.001@gmail.com>
2025-05-08 10:45:48 +0700

    Configure for GitHub Pages deployment

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vietceramics-item/frontend/' // Tên repository của bạn
+    ? '/vietceramics-item/' // Thêm dấu / ở cuối
     : '/',
   outputDir: 'dist',
   assetsDir: 'static',
